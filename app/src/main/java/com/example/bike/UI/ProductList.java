e3f3279009111cb109fed495b5665cfc2bd83990
mikek21 <mikexkiriakou@gmail.com>
2023-01-03 22:05:11 -0500

    initial

---
+++ app/src/main/java/com/example/bike/UI/ProductList.java
@@ -0,0 +1,16 @@
+package com.example.bike.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bike.R;
+
+public class ProductList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_list);
+    }
+}
