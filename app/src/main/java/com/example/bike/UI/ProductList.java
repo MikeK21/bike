020ef22c45d731c55b813238c51137fd5b06bae4
mikek21 <mikexkiriakou@gmail.com>
2023-01-03 22:26:36 -0500

    updates

--- app/src/main/java/com/example/bike/UI/ProductList.java
+++ app/src/main/java/com/example/bike/UI/ProductList.java
@@ -2,9 +2,12 @@ package com.example.bike.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.bike.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ProductList extends AppCompatActivity {
 
@@ -12,5 +15,13 @@ public class ProductList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_list);
+    FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+    fab.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(ProductList.this,ProductDetails.class);
+            startActivity(intent);
+            }
+        });
     }
 }
